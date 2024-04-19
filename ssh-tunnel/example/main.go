97cf86c3dbac2d7886669123f31b040497b2b0ab
superwhys <269085434@qq.com>
2024-04-19 17:44:52 +0800

    update: ssh tunnel unit test

--- ssh-tunnel/example/main.go
+++ ssh-tunnel/example/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 
-	"github.com/superwhys/venkit/ssh-tunnel"
+	sshtunnel "github.com/superwhys/venkit/ssh-tunnel"
 )
 
 func main() {
 	tunnel := sshtunnel.NewTunnel(&sshtunnel.SshConfig{
-		User:         "fdz",
-		HostName:     "s9ga.cnns.net:65522",
-		IdentityFile: "/Users/yong/.ssh/id_rsa_cnns",
+		User:         "yong",
+		HostName:     "10.15.15.123",
+		IdentityFile: "/Users/yong/.ssh/id_rsa",
 	})
 
-	if err := tunnel.Forward(context.TODO(), "localhost:29950", "10.0.0.59:29917"); err != nil {
+	if err := tunnel.Forward(context.TODO(), "localhost:29950", "10.15.15.15.231:80"); err != nil {
 		panic(err)
 	}
 
