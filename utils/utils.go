7b4dbcc5d5023abedaf1b563db017a53a79ed8e8
Hoven <hovenyang.best@gmail.com>
2024-05-11 00:24:34 +0800

    feat: add venkit utils

---
+++ utils/utils.go
@@ -0,0 +1,13 @@
+package venkitUtils
+
+import "os"
+
+func FileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
