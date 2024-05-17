0f1a246258a52b6d068465610e620dbb75a85991
Hoven <hovenyang.best@gmail.com>
2024-05-17 16:33:39 +0800

    feat: add grpc dial with tag

--- dialer/grpc.go
+++ dialer/grpc.go
@@ -15,16 +15,28 @@ func DialGrpc(service string, opts ...grpc.DialOption) (*grpc.ClientConn, error)
 	return DialGrpcWithTimeOut(10*time.Second, service, opts...)
 }
 
+func DialGrpcWithTag(service string, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	return DialGrpcWithTagTimeout(10*time.Second, service, tag, opts...)
+}
+
 func DialGrpcWithTimeOut(timeout time.Duration, service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	return DialGrpcWithContext(ctx, service, opts...)
+}
 
+func DialGrpcWithTagTimeout(timeout time.Duration, service, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	return DialGrpcWithTagContext(ctx, service, tag, opts...)
 }
 
 func DialGrpcWithContext(ctx context.Context, service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	return dialGrpcWithTagContext(ctx, service, "", opts...)
+}
+
+func DialGrpcWithTagContext(ctx context.Context, service string, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	return dialGrpcWithTagContext(ctx, service, tag, opts...)
 }
 
 func dialGrpcWithTagContext(ctx context.Context, service, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
