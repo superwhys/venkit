63973f1d23665a4171a6c93f9d7cfda3e433d6fb
Hoven <hovenyang.best@gmail.com>
2024-06-06 16:56:18 +0800

    update: export workerFunc

--- service/worker.go
+++ service/worker.go
@@ -13,16 +13,18 @@ type worker struct {
 	fn         workerFunc
 }
 
-func WithWorker(fn workerFunc) ServiceOption {
+type WorkerFunc workerFunc
+
+func WithWorker(fn WorkerFunc) ServiceOption {
 	return WithNameWorker(lg.FuncName(fn), fn)
 }
 
-func WithNameWorker(name string, fn workerFunc) ServiceOption {
+func WithNameWorker(name string, fn WorkerFunc) ServiceOption {
 	return func(vs *VkService) {
 		lg.Debugc(vs.ctx, "Add worker. WorkerName=%v", name)
 		vs.workers = append(vs.workers, &worker{
 			name:       name,
-			fn:         fn,
+			fn:         workerFunc(fn),
 			isWithName: lg.FuncName(fn) != name,
 		})
 	}
