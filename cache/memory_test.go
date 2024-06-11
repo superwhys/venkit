4d296c916028313a0bc91ea32c86e12357ae88a8
Hoven <hovenyang.best@gmail.com>
2024-06-11 18:29:32 +0800

    update: memorycache test

--- cache/memory_test.go
+++ cache/memory_test.go
@@ -5,7 +5,21 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/superwhys/venkit/lg"
 )
+
+type People interface {
+	SayHello() string
+}
+
+type Peter struct {
+	name     string
+	timeFunc func() time.Time
+}
+
+func (p *Peter) SayHello() string {
+	return p.name + "" + "hello" + "" + p.timeFunc().String()
+}
 
 func TestMemoryCacheStringValue(t *testing.T) {
 	c := NewMemoryCache(time.Second * 10)
@@ -19,5 +33,27 @@ func TestMemoryCacheStringValue(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "this is a string", resp)
+}
 
+func getPeople(now time.Time) People {
+	return &Peter{
+		name: "everyone",
+		timeFunc: func() time.Time {
+			return now
+		},
+	}
+}
+
+func TestMemoryCacheInterfaceValue(t *testing.T) {
+	now := time.Now()
+	c := NewMemoryCache(time.Second * 10)
+
+	c.Set("interface_key", getPeople(now))
+
+	var resp People
+	err := c.Get("interface_key", &resp)
+	assert.Nil(t, err)
+
+	assert.Equal(t, getPeople(now).SayHello(), resp.SayHello())
+	lg.Info(resp.SayHello())
 }
